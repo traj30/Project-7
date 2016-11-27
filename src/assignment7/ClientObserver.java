81d26d5db6e272fd9f29abb1c08b5efeaea07acc
Tiraj Parikh <Tiraj Parikh>
2016-11-26 19:23:51 -0600

    initial working simple chat

---
+++ src/assignment7/ClientObserver.java
@@ -0,0 +1,31 @@
+/* ClientObserver.java
+ * EE422C Project 4b submission by
+ * Replace <...> with your actual data.
+ * Jake Klovenski
+ * jdk2595
+ * 16445
+ * Tiraj Parikh
+ * trp589
+ * 16460
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
+package assignment7;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Observable;
+import java.util.Observer;
+
+public class ClientObserver extends PrintWriter implements Observer {
+	public ClientObserver(OutputStream out) {
+		super(out);
+	}
+	@Override
+	public void update(Observable o, Object arg) {
+		this.println(arg); //writer.println(arg);
+		this.flush(); //writer.flush();
+	}
+
+}
